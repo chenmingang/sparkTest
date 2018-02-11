f87bf573851b00729e4c224fe0a3c1b5b2d0db6d
zeal <zeal@lagou.com>
2018-02-11 18:22:40 +0800

    tfidf

--- src/main/java/com/github/chenmingang/TfIdf.scala
+++ src/main/java/com/github/chenmingang/TfIdf.scala
@@ -53,4 +53,70 @@ object TfIdf {
     })
 
   }
+
+  def sim(a: String, b: String): Unit = {
+
+  }
+
+
+  def strEditSim(str1: String, str2: String): Double = {
+    val ldNum: Int = ld(str1, str2)
+    val max: Int = Math.max(str1.length, str2.length)
+    1 - ldNum.toDouble / max.toDouble
+  }
+
+  def min(one: Int, two: Int, three: Int): Int = {
+    var min: Int = one
+    if (two < min) {
+      min = two
+    }
+    if (three < min) {
+      min = three
+    }
+    min
+  }
+
+  def ld(str1: String, str2: String): Int = {
+    val n: Int = str1.length
+    val m: Int = str2.length
+    var i: Int = 0
+    var j: Int = 0
+    var ch1: Char = 0
+    var ch2: Char = 0
+    var temp: Int = 0
+    if (n == 0) {
+      return m
+    }
+    if (m == 0) {
+      return n
+    }
+    var d = Array.ofDim[Int](n + 1, m + 1)
+    i = 0
+    while (i <= n) {
+      d(i)(0) = i
+      i += 1
+    }
+    j = 0
+    while (j <= m) {
+      d(0)(j) = j
+      j += 1
+    }
+    i = 1
+    while (i <= n) {
+      ch1 = str1.charAt(i - 1)
+      j = 1
+      while (j <= m) {
+        ch2 = str2.charAt(j - 1)
+        if (ch1 == ch2) {
+          temp = 0
+        } else {
+          temp = 1
+        }
+        d(i)(j) = min(d(i - 1)(j) + 1, d(i)(j - 1) + 1, d(i - 1)(j - 1) + temp)
+        j += 1
+      }
+      i += 1
+    }
+    d(n)(m)
+  }
 }
